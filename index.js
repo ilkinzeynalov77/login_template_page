9942e504add2bd51cd344236baa77986f8d4a8c6
Пользователь <ilkinzeynalov010@gmail.com>
2023-02-08 03:04:16 +0400

    simple page

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function myFunc(){
+   const val=document.querySelector('.field').value;
+   console.log(val);
+   const myEmail=document.querySelector('.gmail').value;
+   console.log(myEmail);
+   const phoneNumber=document.querySelector('.number').value;
+   console.log(phoneNumber);
+   const areaInfo=document.querySelector('.area').value;
+   console.log(areaInfo);
+}
+
